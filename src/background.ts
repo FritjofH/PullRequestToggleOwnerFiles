dabbb48ce7665187b9b43a6e2345e926f7920a9b
Fritjof Höst <fritjof.host@digitalroute.com>
2022-12-08 16:41:18 +0100

    Added logic to detect large amount of changes, even if few files

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,31 @@
 function toggleFilesOwned() {
+  const maxFilesChanged = Number(10);
+  const maxRowsChanged = Number(150);
+
   let checkbox = document.getElementsByName("owned-by[]");
-  if (checkbox.length > 0) {
-    var filesChangedElement = document.getElementById("files_tab_counter")
-    if (filesChangedElement != null){
-      var filesChangedString = filesChangedElement.textContent
-      if (filesChangedString != null){
-        var filesChanged = parseInt(filesChangedString);
-        const onlyOwned = filesChanged > Number(10);
-        if (onlyOwned === true){
-          if (checkbox.item(0).parentElement != null){
-            checkbox.item(0).parentElement?.click()
-          }
-        }
-      }
+  var filesChangedElement = document.getElementById("files_tab_counter");
+  var alreadyClicked = false;
+
+  var filesChangedString = filesChangedElement?.textContent;
+  if (filesChangedString != null){
+    var filesChanged = parseInt(filesChangedString);
+    const onlyOwned = filesChanged > maxFilesChanged;
+    if (onlyOwned === true){
+      checkbox.item(0).parentElement?.click();
+      alreadyClicked = true;
+    }
+  }
+
+  let additions = document.getElementById("diffstat")?.children.item(0);
+  let removals = document.getElementById("diffstat")?.children.item(1);
+  if (alreadyClicked === false
+    && additions?.textContent != null
+    && removals?.textContent != null) {
+    var numOfAdditions = parseInt(additions.textContent.trim().replace('+', ''))
+    var numOfRemovals = parseInt(removals.textContent.trim().replace('−', ''))
+
+    if (numOfAdditions + numOfRemovals > maxRowsChanged){
+      checkbox.item(0).parentElement?.click();
     }
   }
 }
